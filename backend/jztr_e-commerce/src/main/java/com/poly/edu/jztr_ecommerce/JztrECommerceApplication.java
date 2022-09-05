82cc47e5eb4f76f6ca2efebbe573f02d00d3bcb2
xxiihdc <ductran090500@gmail.com>
2022-09-05 11:59:33 +0700

    initialize spring boot application

---
+++ backend/jztr_e-commerce/src/main/java/com/poly/edu/jztr_ecommerce/JztrECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.poly.edu.jztr_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JztrECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JztrECommerceApplication.class, args);
+    }
+
+}
